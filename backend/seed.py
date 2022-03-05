b9d23c228491976c655cae9a0d9598c155d55401
Virginia Wu <virginiawu11@gmail.com>
2022-03-05 14:42:58 -0500

    generate csv from json and seed the database

--- backend/seed.py
+++ backend/seed.py
@@ -1,20 +1,11 @@
 from app import app
 from models import db, Image
+from csv import DictReader
 
 db.drop_all()
 db.create_all()
 
-image1 = Image(
-    url="https://take-home-foam-challenge.s3.us-west-2.amazonaws.com/prod-exp13436-2020-01-08-at-04.24.38-9zijoye9dteugy6agooo506u3c6wrin920a99mavvv4z9mahkt7qbu6thl2l3v39.png",
-    last_modified="2022-02-23T21:31:27.000Z",
-    date_created="2022-02-23T21:31:27.000Z",
-)
-
-
-db.session.add_all(
-    [
-        image1,
-    ]
-)
+with open("generator/images.csv") as images:
+    db.session.bulk_insert_mappings(Image, DictReader(images))
 
 db.session.commit()
