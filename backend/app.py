08cdcf1c39155b25ea326380edf8621207a1f6eb
Virginia Wu <virginiawu11@gmail.com>
2022-03-05 16:45:17 -0500

    add classify image route

--- backend/app.py
+++ backend/app.py
@@ -22,3 +22,8 @@ def get_images():
     "Get Images base on page number"
     return jsonify(ImageService.get_images(1))
 
+@app.route("/images/<id>", methods=["POST"])
+def classify_image(id):
+    "Classify image as foaming or not"
+    is_foaming = request.json.get("isFoaming", None)
+    return jsonify(ImageService.classify_image(id,is_foaming))
--- backend/service.py
+++ backend/service.py
@@ -1,5 +1,5 @@
 from models import db, Image
-# from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 import requests
 
@@ -8,3 +8,11 @@ class ImageService:
     def get_images(cls,page_num):
         images = Image.query.paginate(per_page=10,page=page_num)
         return [Image.serialize(image) for image in images.items]
+
+    @classmethod
+    def classify_image(cls,id,is_foaming):
+        image = Image.query.get(id)
+        image.last_modified = datetime.utcnow()
+        image.is_foaming = is_foaming
+        db.session.commit()
+        return Image.serialize(image)
