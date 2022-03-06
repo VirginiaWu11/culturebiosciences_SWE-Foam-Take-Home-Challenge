16b8752ba9acd66c3133f23eb6a31ccb2ebed828
Virginia Wu <virginiawu11@gmail.com>
2022-03-06 17:32:40 -0500

    move constant variable out

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const CLASSIFICATION = {
+  null: { label: "Unclassified", color: "default" },
+  true: { label: "Foaming", color: "warning" },
+  false: { label: "Not Foaming", color: "primary" },
+};
