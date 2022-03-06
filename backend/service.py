ec3469456f09d1d15ddc53f2dd5a733d45555fb7
Virginia Wu <virginiawu11@gmail.com>
2022-03-05 23:28:33 -0500

    update variable to snake case

--- backend/service.py
+++ backend/service.py
@@ -5,16 +5,16 @@ import requests
 
 class ImageService:
     @classmethod
-    def get_images(cls,page_num = 1,isFoaming = "all",per_page = 5):
+    def get_images(cls,page_num = 1,is_foaming = "all",per_page = 5):
         page_num = int(page_num)
         print("per_page***",per_page)
         per_page = int(per_page)
-        if isFoaming == 'all':
+        if is_foaming == 'all':
             images = Image.query.paginate(per_page=per_page,page=page_num)
-        elif isFoaming == 'None':
+        elif is_foaming == 'None':
             images = Image.query.filter(Image.is_foaming.is_(None)).paginate(per_page=per_page,page=page_num)
         else:
-            images = Image.query.filter_by(is_foaming=isFoaming).paginate(per_page=per_page,page=page_num)
+            images = Image.query.filter_by(is_foaming=is_foaming).paginate(per_page=per_page,page=page_num)
         total_pages = images.pages
         images_list = [Image.serialize(image) for image in images.items]
         return [images_list,total_pages]
